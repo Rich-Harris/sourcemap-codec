f678602d2101b093e2e7fba4e038f5c72c69ccde
Rich-Harris <richard.a.harris@gmail.com>
2015-10-18 11:05:46 -0400

    generate own sourcemaps

--- gobblefile.js
+++ gobblefile.js
@@ -3,6 +3,6 @@ var gobble = require( 'gobble' );
 module.exports = gobble( 'src' ).transform( 'rollup-babel', {
 	entry: 'sourcemap-codec.js',
 	format: 'umd',
-	external: [ 'buffer-crc32' ],
-	moduleName: 'sourcemapCodec'
+	moduleName: 'sourcemapCodec',
+	sourceMap: true
 });
