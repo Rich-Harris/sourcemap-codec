dd2e389195f1e783e2d35ac7299e40aa3285f411
Rich-Harris <richard.a.harris@gmail.com>
2015-10-22 23:01:09 -0400

    add es6 build

--- gobblefile.js
+++ gobblefile.js
@@ -1,8 +1,20 @@
 var gobble = require( 'gobble' );
 
-module.exports = gobble( 'src' ).transform( 'rollup-babel', {
-	entry: 'sourcemap-codec.js',
-	format: 'umd',
-	moduleName: 'sourcemapCodec',
-	sourceMap: true
-});
+module.exports = gobble([
+	// UMD build
+	gobble( 'src' ).transform( 'rollup-babel', {
+		entry: 'sourcemap-codec.js',
+		dest: 'sourcemap-codec.umd.js',
+		format: 'umd',
+		moduleName: 'sourcemapCodec',
+		sourceMap: true
+	}),
+
+	// ES6 build
+	gobble( 'src' ).transform( 'rollup-babel', {
+		entry: 'sourcemap-codec.js',
+		dest: 'sourcemap-codec.es6.js',
+		format: 'es6',
+		sourceMap: true
+	})
+]);
